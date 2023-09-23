4b68f9716f42e42fb9edbb2b9426bcc19eb01ad3
livingfree2023 <125038641+livingfree2023@users.noreply.github.com>
2023-09-23 18:09:17 +0800

    Update bestip.sh

--- bestip.sh
+++ bestip.sh
@@ -8,7 +8,7 @@ echo "------------------"
 date
 echo "------------------"
 
-currentIP=$(nslookup bestip.leosvps.cf 1.1.1.1 | grep "Address: "|awk -F': ' '{ print $2 }')
+currentIP=$(nslookup [****] 1.1.1.1 | grep "Address: "|awk -F': ' '{ print $2 }')
 echo "Current bestip is $currentIP"
 
 /usr/bin/cdnspeedtest \
@@ -20,44 +20,35 @@ echo "Current bestip is $currentIP"
 currentSpeed=$(cat currentspeed.tmp |awk -F',' 'NR==2 {print $6}')
 
 echo "Current speed is $currentSpeed"
+echo "Speed testing..."
 
-
-if [ "$1" == "full" ]; then
-  echo "Speed testing..."
-
-  /usr/bin/cdnspeedtest \
-     -dn 3 \
-     -url [****] \
-     -o /usr/share/cloudflarespeedtestresult.txt \
-     -f /usr/share/CloudflareSpeedTest/ip.txt \
-     -tl 180 \
-     -tll 20 \
-     -tlr 0.2 \
-     -dt 5 \
-     -sl 25 \
-     > /dev/null 2>&1
-
-fi
-
+/usr/bin/cdnspeedtest \
+	-dn 3 \
+	-url [****] \
+	-o /usr/share/cloudflarespeedtestresult.txt \
+	-f /usr/share/CloudflareSpeedTest/ip.txt \
+	-tl 180 \
+	-tll 20 \
+	-tlr 0.2 \
+	-dt 5 \
+	-sl 25 \
+	> /dev/null 2>&1
 
 head /usr/share/cloudflarespeedtestresult.txt
 
 newIP=$(sed -n "2,1p" /usr/share/cloudflarespeedtestresult.txt | awk -F, '{print $1}')
 newSpeed=$(sed -n "2,1p" /usr/share/cloudflarespeedtestresult.txt | awk -F, '{print $6}')
 
-
 if [[ $(($newSpeed-$currentSpeed)) > 0 ]]; then
   echo "New Speed is faster. Updating to DNS: bestip.leosvps.cf"
 
-
-
   response=$(
-    curl -s\
-     -X PUT "https://api.cloudflare.com/client/v4/zones/$cfzone/dns_records/$cfbestipid" \
-	   -H "Authorization: Bearer $cftoken" \
-	   -H "Content-Type: application/json" \
-	    --data '{"type":"A","name":"'$cfbestipname'","content":"'$newIP'","ttl":1,"proxied":false, "Comment":"auto updated by script"}' \
-	    | jq -r '.success')
+		curl -s\
+		-X PUT "https://api.cloudflare.com/client/v4/zones/$cfzone/dns_records/$cfbestipid" \
+		-H "Authorization: Bearer $cftoken" \
+		-H "Content-Type: application/json" \
+		--data '{"type":"A","name":"'$cfbestipname'","content":"'$newIP'","ttl":1,"proxied":false, "Comment":"auto updated by script"}' \
+		| jq -r '.success')
 
   echo "Update successful = $response"
 
